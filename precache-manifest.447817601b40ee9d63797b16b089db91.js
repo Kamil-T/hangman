91fe6dd894ad4954eaf14c494dfe3c7296045b9a
Kamil T <trzaskomakamil@gmail.com>
2019-09-11 18:40:24 +0200

    Updates

---
+++ precache-manifest.447817601b40ee9d63797b16b089db91.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfcde9eb843e598c2e54620e3478f87",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "57257bc408be96b8ffe2",
+    "url": "/hangman/static/css/main.4cd02915.chunk.css"
+  },
+  {
+    "revision": "8aff30586fc3ac60d7a2",
+    "url": "/hangman/static/js/2.256d6241.chunk.js"
+  },
+  {
+    "revision": "57257bc408be96b8ffe2",
+    "url": "/hangman/static/js/main.14af6ba9.chunk.js"
+  },
+  {
+    "revision": "2c05da9272278d863f7b",
+    "url": "/hangman/static/js/runtime~main.67dd2d85.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman/static/media/6.c80394c2.jpg"
+  }
+]);
